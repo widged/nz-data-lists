b1fbf4827c25f916ef341fb767a286e5f991658d
Marielle Lange <mlange@widged.com>
2015-10-22 16:15:05 +1300

    moved screen-capture to separate repo -- see https://github.com/widged/electron-screenshot

--- screen-capture/index.js
+++
@@ -1,6 +0,0 @@
-var ElectronApp = require('../lib/electron/electron_boilerplate/electron_app');
-
-ElectronApp.load(
-  'file://' + __dirname + '/index.html',
-  '../lib/electron/react-devtools'
-);
